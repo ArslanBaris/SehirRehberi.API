c6d9d59d4f746e15f8a988e0e470184ecf513f13
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-24 19:28:18 +0300

    EntityFramework

--- SehirRehberi.API/Controllers/ValuesController.cs
+++ SehirRehberi.API/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Data;
 
 namespace SehirRehberi.API.Controllers
 {
@@ -10,18 +12,27 @@ namespace SehirRehberi.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private DataContext _context;
+        
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<ActionResult> Get(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(v=>v.Id==id);
+            return Ok(value);
         }
 
         // POST api/values
