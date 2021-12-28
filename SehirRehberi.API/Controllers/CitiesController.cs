dc29a9dab6814a0065a429cd83e79e86193368c0
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-28 21:56:03 +0300

    feat: CitiesController.cs added.

---
+++ SehirRehberi.API/Controllers/CitiesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Dtos;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private IAppRepository _appRepository;
+        private IMapper _mapper;
+
+        public CitiesController(IAppRepository appRepository, IMapper mapper)
+        {
+            _appRepository = appRepository;
+            _mapper = mapper;
+        }
+
+        public ActionResult GetCities()
+        {
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CitiyForListDto>>(cities);
+            return Ok(cities);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public ActionResult Add([FromBody] City city)
+        {
+            _appRepository.Add(city);
+            _appRepository.SaveAll();
+            return Ok(city);
+        }
+
+        [HttpGet]
+        [Route("detail")]
+        public ActionResult GetCityById(int id)
+        {
+            var city = _appRepository.GetCityById(id);
+            var cityToReturn = _mapper.Map<CitiyForListDto>(city);
+            return Ok(cityToReturn);
+        }
+    }
+}
